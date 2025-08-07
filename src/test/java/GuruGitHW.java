5df8a09efdbdf7595ccb94939870178e67edbb9d
N.Lukyanova <nadyalukyanovaqa@gmail.com>
2025-08-07 22:31:34 +0400

    first commit

---
+++ src/test/java/GuruGitHW.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GuruGitHW {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("body").shouldHave(text("Мы зарегистрировали подозрительный трафик"));
+    }
+
+    @Test
+    void duckduckgoTest() {
+        open("https://www.duckduckgo.com/");
+        $("[name=q]").setValue("qaguru").pressEnter();
+        $("body").shouldHave(text("qa.guru"));
+    }
+}
